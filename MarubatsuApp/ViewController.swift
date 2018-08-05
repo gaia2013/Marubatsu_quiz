9ee67add8f3580fc24e7254c039f40f6424886c4
Hitoshi <hitoshi.gaia.galaxy@gmail.com>
2018-08-05 14:09:10 +0900

    ○×クイズアプリ

--- MarubatsuApp/ViewController.swift
+++ MarubatsuApp/ViewController.swift
@@ -10,16 +10,92 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var questionLabel: UILabel!
+    
+    // 表示中の問題番号を格納
+    var currentQuestionNum: Int = 0
+    
+    // 問題
+    let questions: [[String: Any]] = [
+        [
+            "question": "iPhoneアプリを開発する統合環境はZcodeである",
+            "answer": false
+        ],
+        [
+            "question": "Xcode画面の右側にはユーティリティー図がある",
+            "answer": true
+        ],
+        [
+            "question": "UILabelは文字列を表示する際に利用する",
+            "answer": true
+        ]
+    ]
+    
+    // 問題を表示する関数
+    func showQuestion() {
+        let question = questions[currentQuestionNum]
+        
+        // "question"というkeyで紐づいているvalueがString型であるなら〜
+        if let que = question["question"] as? String {
+            questionLabel.text = que
+        }
+    }
+    
+    // 回答をチェックする関数
+    // 正解なら次の問題を表示します
+    func checkAnswer(yourAnswer: Bool) {
+        
+        let question = questions[currentQuestionNum]
+        
+        if let ans = question["answer"] as? Bool {
+            
+            if yourAnswer == ans {
+                // 正解
+                // currentQuestionNumを1足して次の問題に進む
+                currentQuestionNum += 1
+                showAlert(message: "正解！")
+            } else {
+                // 不正解
+                showAlert(message: "不正解…")
+                currentQuestionNum += 1
+            }
+        } else {
+            print("答えが入っていません")
+            return
+        }
+        
+        // 問題表示前にcurrentQuestionNumの値が「問題数以上（∵配列は[0]始まり）」だったら最初の問題に戻す
+        if currentQuestionNum >= questions.count {
+            currentQuestionNum = 0
+        }
+        
+        // 問題を表示します。
+        // 正解であれば次の問題が、不正解であれば同じ問題が再表示されます。
+        showQuestion()
+    }
+    
+    // アラートを表示する関数
+    func showAlert(message: String) {
+        let alert = UIAlertController(title: nil, message: message, preferredStyle: .alert)
+        let close = UIAlertAction(title: "閉じる", style: .cancel, handler: nil)
+        alert.addAction(close)
+        present(alert, animated: true, completion: nil)
+    }
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
-    }
-
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
+        
+        showQuestion()
     }
 
 
+
+    @IBAction func tappedNoButton(_ sender: UIButton) {
+        checkAnswer(yourAnswer: false)
+    }
+    
+    @IBAction func tappedYesButton(_ sender: UIButton) {
+        checkAnswer(yourAnswer: true)
+    }
 }
 
